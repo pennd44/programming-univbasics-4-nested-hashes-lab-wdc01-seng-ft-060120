c2f016845ba04b899e268827402c9aabf0699907
Penn M Diba <pdiba@oxbowschool.org>
2020-05-25 01:55:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -36,7 +36,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-  puts programmer_hash[:alan_kay][:known_for]
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,7 +55,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-  puts programmer_hash[:dennis_ritchie][:languages][0]
+  programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -82,7 +82,7 @@ def adding_matz
       }
     }
   programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
-  puts programmer_hash
+  programmer_hash
 end
 
 def changing_alan
@@ -105,7 +105,7 @@ def changing_alan
       }
     }
   programmer_hash [:alan_kay][:known_for]= "GUI"
-  puts programmer_hash
+  programmer_hash
 end
 
 def adding_to_dennis
@@ -128,5 +128,5 @@ def adding_to_dennis
       }
     }
   programmer_hash[:dennis_ritchie][:languages] >> "Assembly"
-  puts programmer_hash
+  programmer_hash
 end
